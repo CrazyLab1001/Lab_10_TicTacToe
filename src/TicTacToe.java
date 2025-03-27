3036104aa7a70ea6832f0018ea68c229f31cfc13
CrazyLab1001 <cramerkj@mail.uc.edu>
2025-03-27 19:05:00 -0400

    Made/Wrote clearBoard and display

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,7 +1,76 @@
+import java.util.Scanner;
+
 public class TicTacToe {
+    private static final int ROW = 3;
+
+    private static final int COL = 3;
+
+    private static String board [][] = new String[ROW][COL];
+
+
     public static void main(String[] args) {
+        int row = 0;
+        int col = 0;
+        int moveCount = 0;
+        boolean stillPlaying = true;
+        String player = "X";
+        do {
+            do {
+                clearBoard();
+                display();
+                System.out.println("What's your move," + player + "?");
+                Scanner in = new Scanner(System.in);
+                row = SafeInput.getRangedInt(in, "Enter row", 1, 3);
+                col = SafeInput.getRangedInt(in, "Enter row", 1, 3);
+            } while (!isValidMove(row, col));
 
-        System.out.println("Hello, World!");
+        } while (!stillPlaying);
 
+    }
+
+
+
+    private static void clearBoard() { // all board spaces are empty!
+        { for (int row = 0; row < ROW; row++) {
+            for (int col = 0; col < COL; col++){
+                board[row][col] = " "; // clears space
+            }
+            System.out.println();
+        }
+    } }
+
+    private static void display()
+    { // shows game as it's played. :) (also is used as prompt for moves)
+            { for (int row = 0; row < ROW; row++) {
+                System.out.print("| ");
+                for (int col = 0; col < COL; col++){
+                    System.out.print(board[row][col] + " | "); }
+
+                System.out.println("");}
+    }
+        }
+
+        private static boolean isValidMove(int row, int col) { // this checks if a player is in the same spot as requested move
+        return false;
+    }
+
+    private static boolean isWin(String player) { // goes through all win classes to make sure all are false (or if one is true!)
+return false;
+    }
+
+    private static boolean isColWin(String player) { // checks columns for win
+return false;
+    }
+
+    private static boolean isRowWin(String player) { // checks rows for win
+return false;
+    }
+
+    private static boolean isDiagnalWin (String player) { // checks for diagnal win (hard coded)
+return false;
+    }
+
+    private static boolean isTie() { // checks for ties!
+return false;
     }
 }
